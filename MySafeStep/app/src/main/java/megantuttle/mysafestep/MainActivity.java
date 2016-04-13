58be5a7d3edc7cb35bed68d4fe6798644b456f26
megantuttle <tuttlem21@gmail.com>
2016-04-12 22:19:16 -0400

    coordinator layout almost there

--- MySafeStep/app/src/main/java/megantuttle/mysafestep/MainActivity.java
+++ MySafeStep/app/src/main/java/megantuttle/mysafestep/MainActivity.java
@@ -1,16 +1,22 @@
 package megantuttle.mysafestep;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -18,13 +24,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
+        //image = (ImageView) findViewById(R.id.imageView_icon);
+        //image.setImageResource(R.drawable.ic_ss_icon);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("My SafeStep");
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+        collapsingToolbarLayout.setContentScrimColor(Color.BLUE);
+        collapsingToolbarLayout.setStatusBarScrimColor(Color.GREEN);
+
+
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        **/
     }
 
     @Override
@@ -57,19 +78,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-
-    //Defining stuff
-    public final static String EXTRA_MESSAGE = "megantuttle.mysafestep.MESSAGE";
-
-    //Called when the user clicks the Send button, the other activity pops up on screen
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 
     //Make a new activity to be called when a user clicks the Gait Tracker button
     public void gaitTracker(View view){
