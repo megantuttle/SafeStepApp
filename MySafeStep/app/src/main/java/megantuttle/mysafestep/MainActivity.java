e2ff72ecc4877463742b83827b829558dac3ce16
megantuttle <tuttlem21@gmail.com>
2016-04-15 10:20:42 -0400

    fixed image issue

--- MySafeStep/app/src/main/java/megantuttle/mysafestep/MainActivity.java
+++ MySafeStep/app/src/main/java/megantuttle/mysafestep/MainActivity.java
@@ -71,17 +71,6 @@ public class MainActivity extends AppCompatActivity
         mAppBarLayout   = (AppBarLayout) findViewById(R.id.main_appbar);
     }
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        **/
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
